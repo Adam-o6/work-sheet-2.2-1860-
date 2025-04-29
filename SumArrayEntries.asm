31c2fc3f77461e5316fec3ceec1711d696bb6571
Adam-o6 <vxff0627@leeds.ac.uk>
2025-04-29 23:58:50 +0000

    w

--- SumArrayEntries.asm
+++ SumArrayEntries.asm
@@ -1,50 +1,53 @@
-@R2 
-M = 0 
+// R2 == 0
+@R2
+M=0
 
-@R0     
+// Check if R1 <= 0
+@R1
+D=M        // D = R1
+@END
+D;JLE      // If R1 <= 0, jump to END
 
-D = M 
+// R3 to store array pointer
+@R0
+D=M
+@R3
+M=D        // R3 = R0
 
-@R8 
+// R4 to store loop counter
+@R1
+D=M
+@R4
+M=D        // R4 = R1
 
-M = D 
-
-@R1 
-
-D = M 
-
-@R7
-M = D 
-
+// Loop start
 (LOOP)
+@R4
+D=M
+@END
+D;JEQ      // If counter == 0, end loop
 
-    @R8
+// Load value at current array address into D
+@R3
+A=M
+D=M        // D = *R3
 
-    D = M 
+// Add D to R2
+@R2
+M=M+D
 
-    @R2
+// Increment R3
+@R3
+M=M+1
 
-    M = M + D 
+// Decrement R4
+@R4
+M=M-1
 
-    @R8
+@LOOP
+0;JMP      // Repeat loop
 
-    M = M + 1 
-    
- 
-    @R7
-    D=M     
-    D=D-1   
-    M=D 
-
-    @END_LOOP 
-
-    D;JLE
-
-    @LOOP
-
-
-(END_LOOP)
-
-@END_LOOP
-
-0;JMP
+// End label
+(END)
+@END
+0;JMP      // Infinite loop
